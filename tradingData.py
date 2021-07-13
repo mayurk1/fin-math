d274165ec6d21621830e5457233098b55a30b862
Mayur Khanna <82302692+mayurk2@users.noreply.github.com>
2021-07-12 22:33:45 -0500

    Add files via upload
    Class for extracting price data using the Alpaca API

---
+++ tradingData.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+AFG
+Created 10 July 2021
+Updated 11 July 2021
+@author: Mayur
+"""
+
+# Requires a config.py file containing the 
+# below variables with alpaca account API keys
+# and url
+from config import api_key, api_secret, base_url
+import alpaca_trade_api as tradeapi
+# Instantiate REST API
+api = tradeapi.REST(api_key, api_secret, base_url, api_version='v2')
+
+class histData: 
+    def daily(ticker, delta):
+        # Request historic data from alpaca API into
+        # a pandas dataframe of daily closes delta 
+        # days from current days (dataset has nontrading
+        # days already removed) 
+        stock = api.get_barset(ticker, 'day', limit=delta).df
+        stock.columns = stock.columns.droplevel()
+
+        return stock
+
+    #WIP
+    # TODO create time interval functions 
+    def min15(ticker, delta):
+        # Request historic data from alpaca API into
+        # a pandas dataframe
+        stock = api.get_barset(ticker, 'minute', limit=delta).df
+        stock.columns = stock.columns.droplevel()
+
+        return stock
